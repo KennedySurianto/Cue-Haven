17ff346ce3aa90a7c4da0cb97df1bda2e51a4967
KennedySurianto <kennedysrnt@gmail.com>
2024-06-20 05:38:24 +0700

    update: query.sql

--- query.sql
+++ query.sql
@@ -6,3 +6,15 @@ CREATE TABLE Transactions (
 	start_time TIME NOT NULL,
 	end_time TIME NOT NULL
 );
+
+SELECT 
+	transaction_id, 
+	transaction_date, 
+	customer_name, 
+	table_number, 
+	start_time, 
+	end_time, 
+	end_time - start_time AS play_time, 
+	ROUND((EXTRACT(EPOCH FROM (end_time - start_time)) / 3600.0) * 50000, 0) AS charge
+FROM Transactions
+ORDER BY transaction_id DESC;
